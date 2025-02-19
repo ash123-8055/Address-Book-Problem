46c859e994e7415caac3e4e7042a50d261913e4c
Ashwin <ash123.8055@gmail.com>
2025-02-19 17:40:54 +0530

    [Ashwin] Add: Added the ability to add multiple address books to the system with unique address book names.

--- contacts.py
+++ contacts.py
@@ -31,7 +31,8 @@ class AddressBookMain:
         self.zipcode=zipcode
         self.phone_number=phone_number
         self.email=email
-        self.address_book=dict()
+        self.address_book={}
+        self.system_book={}
         self.contact_dict={
             "Address":self.address,
             "City":self.city,
@@ -41,7 +42,13 @@ class AddressBookMain:
             "Email":self.email
         }
         self.address_book[self.first_name+" "+self.last_name]=self.contact_dict
-        logger.info("Created the dictionary successfully.")
+        self.address_book_name=input("Enter the name of the address book your adding the contact to: ")
+        if self.address_book_name not in self.system_book:
+            self.system_book[self.address_book_name] = {}
+
+        self.system_book[self.address_book_name].update(self.address_book)
+        self.address_book={}
+        logger.info("Uploaded a contact into the system")
 
     def except_hand(self):
         """
@@ -69,17 +76,19 @@ class AddressBookMain:
         Return: None
         """
 
-        if not self.address_book:
+        if not self.system_book:
             print("The Address book is empty!!")
             logger.warning("Trying to print a empty Address Book.")
 
         print()
-        for key,value in self.address_book.items():
-            print(f"\nName - {key}")
+        for key,value in self.system_book.items():
+            print(f"\n{key}")
             for key1,value1 in value.items():
-                print(f"{key1} - {value1}")
+                print(f"\nName - {key1}")
+                for key2,value2 in value1.items():
+                    print(f"{key2} - {value2}")
         
-        logger.info("Printed the details successfully.")
+        logger.info("Printed the Contact details successfully.")
 
     def update_contact(self):
         """
@@ -120,6 +129,12 @@ class AddressBookMain:
             }
 
         self.address_book[self.first_name+" "+self.last_name]=self.contact_dict
+        self.address_book_name=input("Enter the name of the address book your adding the contact to: ")
+        if self.address_book_name not in self.system_book:
+            self.system_book[self.address_book_name] = {}
+
+        self.system_book[self.address_book_name].update(self.address_book)
+        self.address_book={}
         logger.info("Updated the dictionary successfully.")
         print("Updated the Dictionary!!\n")
 
@@ -132,21 +147,49 @@ class AddressBookMain:
         Return: None
         """
 
-        if not self.address_book:
+        if not self.system_book:
             print("The Address Book is empty!! Cant delete anything..")
             logger.warning("The Address is Empty and tried to delete a contact.")
         
         else:
-            print("Enter the first name and last name to delete the contact..")
-            self.first_name=input("Enter the First name: ")
-            self.last_name=input("Enter the Last name: ")
-            if (self.first_name+" "+self.last_name) in self.address_book.keys():
-                self.address_book.pop(self.first_name+" "+self.last_name)
-                print(f"\nRemoved {self.first_name+" "+self.last_name} from the Address Book.")
-                logger.info("Removed a contact from Address Book.")
+            self.print_address_book()
+            self.address_book_name=input("Enter the address book name you want to delete the contact from: ")
+            if self.address_book_name in self.system_book.keys():
+                print("\nEnter the first name and last name to delete the contact..")
+                self.first_name=input("Enter the First name: ")
+                self.last_name=input("Enter the Last name: ")
+                full_name=self.first_name+" "+self.last_name
+                if full_name in self.system_book[self.address_book_name]:
+                    self.system_book[self.address_book_name].pop(full_name)
+                    print(f"\nRemoved {self.first_name+" "+self.last_name} from the Address Book.")
+                    logger.info("Removed a contact from Address Book.")
 
+                else:
+                    print(f"The {self.first_name+" "+self.last_name} was not in the Address Book.")
+                    logger.warning("Tried to delete a contact which wasn't there in the Address Book.")
+            
             else:
-                print(f"The {self.first_name+" "+self.last_name} was not in the Address Book.")
+                print("The Address book is not in the system. ")
+                logger.warning("The User tried accessing a address book which doesn't exist.")
+
+    def print_address_book(self):
+        """
+        Description: This function is used to print the available Address book in the system
+
+        Parameter: Self: Object
+
+        Return: Prints the available Address Book
+        """
+
+        if not self.system_book:
+            print("The System doesnt have any Address book.")
+            logger.info("The System has no Address book!")
+        
+        else:
+            print("\nThe Address book in the system as follow:\n")
+            for key in self.system_book.keys():
+                print(f"{key}")
+            logger.info("The Address Book available in the system was printed.")
 
 def main():
     """
@@ -158,6 +201,7 @@ def main():
     """
 
     try:
+        logger.info("Started the Script!!!")
         print("\nWelcome to the Address Book:\n")
         first_name=input("Enter the first name: ")
         last_name=input("Enter the last name: ")
@@ -177,31 +221,35 @@ def main():
             logger.error("Error on the phone number.")
         
         contact_one=AddressBookMain(first_name,last_name,address,city,state,zipcode,phone_number,email)
-        print("\nEnter the choice:\n1. Print the Contact.\n2. Update the Contact.\n3. Delete contact\n4. Exit")
+        print("\nEnter the choice:\n1. Print the Contact book.\n2. Print the Contact Details.\n3. Update the Contact.\n4. Delete contact.\n5. Exit")
         choice=input("\nThe Choice: ")
 
         while True:
             match choice:
                 case "1":
+                    contact_one.print_address_book()
+
+                case "2":
                     contact_one.print_contact()
                 
-                case "2":
+                case "3":
                     counter=int(input("How many contacts you have decided to add: "))
                     logger.info(f"The user has decided to add {counter} contacts")
                     for i in range(counter):
                         contact_one.update_contact()
                 
-                case "3":
+                case "4":
                     contact_one.delete_contact()
 
-                case "4":
+                case "5":
                     print("Exiting the program!!!")
+                    logger.info("Closed the Script!!!!")
                     break
 
                 case default:
                     print("\nInvalid Input!\nTry the other options available!")
 
-            print("\nEnter the choice:\n1. Print the Contact.\n2. Update the Contact.\n3. Delete contact\n4. Exit")
+            print("\nEnter the choice:\n1. Print the Contact book.\n2. Print the Contact Details.\n3. Update the Contact.\n4. Delete contact.\n5. Exit")
             choice=input("\nThe Choice: ")
 
     except ValueError as ve:
